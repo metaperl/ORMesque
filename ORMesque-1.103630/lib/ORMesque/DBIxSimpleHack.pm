3663d811c037d87854110b0a2c3fa66438da496f
Al Newkirk <we@ana.im>
2010-12-28 21:29:12 -0500

    fixed plan order of various tests, fixed disconnect cleanup operation in DBIxSimpleHack

--- ORMesque-1.103630/lib/ORMesque/DBIxSimpleHack.pm
+++ ORMesque-1.103630/lib/ORMesque/DBIxSimpleHack.pm
@@ -503,3 +503,27 @@
     package #Hide from PAUSE
 	ORMesque::DBIxSimpleHack;
 1;
+__END__
+=pod
+
+=head1 NAME
+
+DBIx::Simple
+
+=head1 VERSION
+
+version 1.103630
+
+=head1 AUTHOR
+
+Al Newkirk <awncorp@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by awncorp.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
